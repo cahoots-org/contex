05965c396fd62a469c594bfca97b268f3acae2b5
Rob Miller <robmillersoftware@gmail.com>
2026-02-10 23:13:39 -0500

    Deploying opensearch image from repo

--- opensearch/entrypoint.sh
+++ opensearch/entrypoint.sh
@@ -2,5 +2,5 @@
 # Fix volume permissions for Railway
 chown -R opensearch:opensearch /usr/share/opensearch/data 2>/dev/null || true
 
-# Switch to opensearch user and run
-exec setpriv --reuid=opensearch --regid=opensearch --init-groups /usr/share/opensearch/opensearch-docker-entrypoint.sh
+# Drop to opensearch user and run
+exec setpriv --reuid=opensearch --regid=opensearch --init-groups -- /usr/share/opensearch/opensearch-docker-entrypoint.sh
