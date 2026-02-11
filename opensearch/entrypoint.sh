c5fdf40920d112fecd6243a6a33e568eaa07b422
Rob Miller <robmillersoftware@gmail.com>
2026-02-11 06:59:06 -0500

    Still trying to fix opensearch

--- opensearch/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Fix volume permissions for Railway
-chown -R opensearch:opensearch /usr/share/opensearch/data 2>/dev/null || true
-
-# Drop to opensearch user and run
-exec setpriv --reuid=opensearch --regid=opensearch --keep-groups -- /usr/share/opensearch/opensearch-docker-entrypoint.sh
